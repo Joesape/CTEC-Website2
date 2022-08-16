62bf9014f51d0ecc22277e57bb7382c68266c66e
Joesape <Josephtjy@gmail.com>
2022-08-16 16:26:12 +0800

    ctec website

---
+++ Restaurant review website/public/js/app.js
@@ -0,0 +1,13 @@
+var restaurant_url = "/restaurantdetails";
+var restaurant_array = []; // This creates an empty movie array
+var restaurantCount = 0;
+/*  There are two categories: "Now Showing" and "Coming Soon". This variable states which 
+    category of movies should be listed when the home page is first loaded. */
+var category = "restaurantdetails";
+var currentIndex=0;
+var review_url = "/restaurantreview";
+var review_array = []; // This creates an empty review array
+var popcornBWImage = 'images/popcorn_bw.png';
+var popcornImage = 'images/popcorn.png';
+var rating = 0;
+var transurl = new URL("")
